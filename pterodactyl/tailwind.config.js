280eb003a162efa623fac80d54541224537616c8
VantaHost <mehraronak81-creator@users.noreply.github.com>
2026-07-26 15:19:50 +0530

    Restore compatible Tailwind line clamp prebuild

--- pterodactyl/tailwind.config.js
+++ pterodactyl/tailwind.config.js
@@ -1,16 +1,16 @@
 const colors = require('tailwindcss/colors');
 
 const gray = {
-    50: 'var(--gray50)',
-    100: 'var(--gray100)',
-    200: 'var(--gray200)',
-    300: 'var(--gray300)',
-    400: 'var(--gray400)',
-    500: 'var(--gray500)',
-    600: 'var(--gray600)',
-    700: 'var(--gray700)',
-    800: 'var(--gray800)',
-    900: 'var(--gray900)',
+    50: 'hsl(216, 33%, 97%)',
+    100: 'hsl(214, 15%, 91%)',
+    200: 'hsl(210, 16%, 82%)',
+    300: 'hsl(211, 13%, 65%)',
+    400: 'hsl(211, 10%, 53%)',
+    500: 'hsl(211, 12%, 43%)',
+    600: 'hsl(209, 14%, 37%)',
+    700: 'hsl(209, 18%, 30%)',
+    800: 'hsl(209, 20%, 25%)',
+    900: 'hsl(210, 24%, 16%)',
 };
 
 module.exports = {
@@ -22,30 +22,10 @@ module.exports = {
             fontFamily: {
                 header: ['"IBM Plex Sans"', '"Roboto"', 'system-ui', 'sans-serif'],
             },
-            borderRadius: {
-                box: 'var(--radiusBox)',
-                component: 'var(--radiusInput)'
-            },
             colors: {
                 black: '#131a20',
                 // "primary" and "neutral" are deprecated, prefer the use of "blue" and "gray"
                 // in new code.
-                vantablack: 'var(--primary)',
-                success: {
-                    50: 'var(--successText)',
-                    100: 'var(--successBorder)',
-                    200: 'var(--successBackground)'
-                },
-                danger: {
-                    50: 'var(--dangerText)',
-                    100: 'var(--dangerBorder)',
-                    200: 'var(--dangerBackground)'
-                },
-                secondary: {
-                    50: 'var(--secondaryText)',
-                    100: 'var(--secondaryBorder)',
-                    200: 'var(--secondaryBackground)'
-                },
                 primary: colors.blue,
                 gray: gray,
                 neutral: gray,
@@ -63,6 +43,7 @@ module.exports = {
         },
     },
     plugins: [
+        require('@tailwindcss/line-clamp'),
         require('@tailwindcss/forms')({
             strategy: 'class',
         }),
